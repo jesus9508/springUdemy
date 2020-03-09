446deb13f0015370c8e4b2713bd30a8e69034c45
root <root@debian>
2020-03-09 11:04:35 -0500

    ultima modiicacion curso

--- springboot-servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ springboot-servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SpringbootServicioEurekaServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
+	}
 
 }
--- springboot-servicio-zuul-server/src/test/java/com/formacionbdi/springboot/app/zuul/SpringbootServicioZuulServerApplicationTests.java
+++ springboot-servicio-zuul-server/src/test/java/com/formacionbdi/springboot/app/zuul/SpringbootServicioZuulServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.formacionbdi.springboot.app.eureka;
+package com.formacionbdi.springboot.app.zuul;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootServicioEurekaServerApplicationTests {
+public class SpringbootServicioZuulServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
