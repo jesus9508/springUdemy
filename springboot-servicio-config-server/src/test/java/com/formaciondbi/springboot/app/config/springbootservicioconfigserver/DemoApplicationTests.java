1d41cb88e7176006c184786013b2c515dd6cb35e
root <root@debian>
2020-03-09 12:47:01 -0500

    agragando configuracion

---
+++ springboot-servicio-config-server/src/test/java/com/formaciondbi/springboot/app/config/springbootservicioconfigserver/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaciondbi.springboot.app.config.springbootservicioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
